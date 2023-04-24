91ca91d41e06802b3990f134b044934351501ec5
Jakub Werwinski <werwa999@wp.pl>
2023-04-24 22:33:46 +0200

    Add DockerImage

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Python and Flask application focused on building a platform to analyze and transcribe any Audio File uploaded to the website.
 
 This application take an Audio File as input in Flask to create both a GET and POST request on the same route and finally render the transcribed results of the speech file to the user.
+
+Update Docker 
+
+How to run: 
+> docker build --tag transcriber .  
+
+> docker run --publish 80:5000 transcriber 
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, send_file
 import stt
+
 app = Flask(__name__)
 
 
@@ -18,23 +19,19 @@ def index():
             return redirect(request.url)
 
         if file:
-            msl = int(request.form['min_silence_len'])
-            st = int(request.form['silence_thresh'])
-            ks = int(request.form['keep_silence'])
-            lang = str(request.form['lang'])
+            msl = int(request.form["min_silence_len"])
+            st = int(request.form["silence_thresh"])
+            ks = int(request.form["keep_silence"])
+            lang = str(request.form["lang"])
             value = stt.get_large_audio_transcription(file, msl, st, ks, lang)
             error = stt.errorcount
-            with open('transcriber.txt', 'w') as f:
+            with open("transcriber.txt", "w") as f:
                 f.write(value)
 
-    return render_template('index.html', transcript=value, error=error)
+    return render_template("index.html", transcript=value, error=error)
 
 
-@app.route('/download')
+@app.route("/download")
 def download_file():
     path = "transcriber.txt"
     return send_file(path, as_attachment=True)
-
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
